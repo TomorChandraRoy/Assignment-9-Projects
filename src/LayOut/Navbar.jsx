5a26d49984f1885c5da04d0a197000c8ec952804
TomorChandraRoy <tomorchandraroy884@gmail.com>
2023-10-09 22:03:03 +0600

    Nav section complete

--- src/LayOut/Navbar.jsx
+++ src/LayOut/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
         <>
           <div className="flex lg:gap-4">
             <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/login">Login</NavLink></li>
             <li><NavLink to="/event">Events</NavLink></li>
+            <li><NavLink to="/login">Login</NavLink></li>
         
             { user &&
               <>
